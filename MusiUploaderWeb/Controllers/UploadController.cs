98056f09b4135e7da65ff2affad1e675dd515a59
ivan.liovic <ivan.liovic9@gmail.com>
2017-08-29 21:51:03 +0200

    Adding the web application to the solution

---
+++ MusiUploaderWeb/Controllers/UploadController.cs
@@ -0,0 +1,25 @@
+﻿using MusiUploaderWeb.Models.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MusiUploaderWeb.Controllers
+{
+    public class UploadController : Controller
+    {
+        // GET: Upload
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult UploadFile(UploadViewModel model)
+        {
+            var file = model.File;
+
+            return View(model);
+        }
+    }
+}
---
+++ MusiUploaderWeb/Models/ViewModel/UploadViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MusiUploaderWeb.Models.ViewModel
+{
+    public class UploadViewModel
+    {
+        public HttpPostedFileBase File { get; set; }
+        public string FileName { get; set; }
+    }
+}
