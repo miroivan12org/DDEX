11a847608b4c015515d4e6ebd2023affb5ff8d25
ivan.liovic <ivan.liovic9@gmail.com>
2017-09-12 22:23:55 +0200

    Basic jQuery validation added on wizard.

---
+++ MusiUploaderWeb/Scripts/Custom/WizardValidator.js
@@ -0,0 +1,42 @@
+﻿$(document).ready(function () {
+    var $validator = $("#commentForm").validate({
+        rules: {
+            title: {
+                required: true,
+                email: true,
+                minlength: 3
+            },
+            namefield: {
+                required: true,
+                minlength: 3
+            },
+            urlfield: {
+                required: true,
+                minlength: 3,
+                url: true
+            }
+        }
+    });
+
+    $("#wizard").bootstrapWizard({
+        onTabShow: function (tab, navigation, index) {
+            var $total = navigation.find('li').length;
+            var $current = index + 1;
+            var $percent = ($current / $total) * 100;
+            $('#wizard .progress-bar').css({ width: $percent + '%' });
+        },
+        onTabClick: function (tab, navigation, index) {
+            return false;
+        },
+        //'nextSelector': 'button-next',
+        //'previousSelector': 'button-previous',
+        onNext: function (tab, navigation, index) {
+            //validacija napravi funckiju koja će okidati validate forme
+            var $valid = $("#commentForm").valid();
+            if (!$valid) {
+                $validator.focusInvalid();
+                return false;
+            }
+        }
+    });
+});
